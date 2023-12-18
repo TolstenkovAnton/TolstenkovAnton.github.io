00cc1a05cc057c1d58fcbb8ea6e28ba813353cba
TolstenkovAnton <kykyRUSka2004@gmail.com>
2023-12-18 23:22:38 +0300

    Fixed index6.html

--- calculator6.js
+++ calculator6.js
@@ -14,29 +14,29 @@ function display_off(elem_id)
 
 function backpack_extend()
 {
-    display_on('backpack_add');
-    display_off('pen_add');
+    display_on("backpack_add");
+    display_off("pen_add");
     checked_type = 0;
 }
 
 function pen_extend()
 {
-    display_on('pen_add');
-    display_off('backpack_add');
+    display_on("pen_add");
+    display_off("backpack_add");
     checked_type = 1;
 }
 
 function pencilbox_extend()
 {
-    display_off('pen_add');
-    display_off('backpack_add');
+    display_off("pen_add");
+    display_off("backpack_add");
     checked_type = 2;
 }
 
 function calculate_price_items()
 {
     let n = document.getElementById("number_items").value;
-    if (!isNaN(n) && parseFloat(parseInt(n)) == parseFloat(n) && n >= 1)
+    if (!Number.isNaN(n) && parseFloat(parseInt(n)) == parseFloat(n) && n >= 1)
     {
         console.log(checked_type);
         switch (checked_type)
@@ -45,9 +45,9 @@ function calculate_price_items()
             {
                 console.log("case0");
                 let cost = 3990 * n;
-                let checkboxes = document.querySelectorAll('.checkbox_backpack input[type="checkbox"]');
+                let checkboxes = document.querySelectorAll(".checkbox_backpack input[type='checkbox']");
                 console.log(checkboxes);
-                for (let i = 0; i < 4; i++) 
+                for (let i = 0; i < checkboxes.length; i++) 
                 {
                     if (checkboxes[i].checked) 
                     {
@@ -55,7 +55,7 @@ function calculate_price_items()
                     }
                 }
                 cost = cost.toFixed(2);
-                document.getElementById("result_items").textContent = cost + " руб.";
+                document.getElementById("result").textContent = cost + " руб.";
                 break;
             }
             case 1:
@@ -63,7 +63,7 @@ function calculate_price_items()
                 console.log("case1");
                 let cost = document.getElementById("colors").value * n;
                 cost = cost.toFixed(2);
-                document.getElementById("result_items").textContent = cost + " руб.";
+                document.getElementById("result").textContent = cost + " руб.";
                 break;
             }
             case 2:
@@ -71,13 +71,13 @@ function calculate_price_items()
                 console.log("case2");
                 let cost = 400 * n;
                 cost = cost.toFixed(2);
-                document.getElementById("result_items").textContent = cost + " руб.";
+                document.getElementById("result").textContent = cost + " руб.";
                 break;
             }
         }
     }
     else
     {
-        document.getElementById("result_items").textContent = "Ошибка ввода";
+        document.getElementById("result").textContent = "Ошибка ввода";
     }
 }
