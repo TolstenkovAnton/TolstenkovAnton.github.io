280ba5b87deea279925b4a466e72116ac679d2c5
TolstenkovAnton <kykyRUSka2004@gmail.com>
2024-05-27 20:05:04 +0300

    update task3

--- be/task3/body.php
+++ be/task3/body.php
@@ -3,71 +3,67 @@
 
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="../style.css">
     <link rel="stylesheet" type="text/css" href="style.css">
-    <link rel="shortcut icon" href="../../logo.png" />
+    <link rel="shortcut icon" href="../../icon.png" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet">
     <title>Задание 3</title>
 </head>
-
 <body>
-<header>
-    <nav class="header-logo">
-        <img class="header-logo" src="../../logo.png" alt="Logo">
-    </nav>
-    <nav class="header-info">
-        <h2 class="header-title">$$$ website?</h2>
-        <h3 class="header-task">Задание 3</h3>
-    </nav>
+    <header class = "rounded-bottom"> 
+        <img src="icon.png" alt = "logo" class = "logo"> 
+        <div id = "name">RECORDING LABELS</div> 
+    </header>
 
-</header>
-
-<aside>
-    <nav class="aside-link">
-        <a href="../index.html" class="">Задания 4-го семестра</a>
-    </nav>
-</aside>
-
-<main>
-    <div class="tasks" id="div_form">
-        <form method="post" id="form">
-            <h1 id="h1-form" class="black">Форма</h1>
-            <div id="div-result">
+    <div class = "block_form rounded mt-2 pl-4 pb-4">
+          <h1 class = "mt-2">Форма</h1>
+          <form action="#" method="post" target="_blank">
+            <fieldset>
+              <legend>Персональные данные</legend>
+              <ul>
+                <li>
+                  <label for="fio">ФИО:</label>
+                  <input type="text" name="fio" placeholder="Иван Иванов Иванович" id="fio" required>
+                </li>
+                <li>
+                    <label for="bday">Дата рождения: </label>
+                    <input type="date" id="bday" name="bday">
+                </li>
+              </ul>
+            </fieldset>
+            <fieldset>
+                <legend>Пол</legend>
+                <ul>
+                  <li>
+                    <input type="radio" name="sex" id="front" value="male" checked>
+                    <label for="front">Мужской</label>
+                  </li>
+                  <li>
+                    <input type="radio" name="sex" id="back" value="female">
+                    <label for="back">Женский</label>
+                  </li>
+                </ul>
+              </fieldset>
+            <fieldset>
+              <legend>Контакты</legend>
+              <ul>
+                <li>
+                  <label for="email">E-mail:</label>
+                  <input type="email" name="email" placeholder="ivanov@gmail.com" id="email" required>
+                </li>
+                <li>
+                  <label for="telephone">Телефон:</label>
+                  <input type="tel" name="telephone" placeholder="+7 000 000-00-00" id="telephone" maxlength="21" required>
+                </li>
+              </ul>
+            </fieldset>
+            <div>
+              <label for="message">Биография</label>
+              <br>
+              <textarea name="biography" placeholder="Расскажите о себе" id="message"></textarea>
             </div>
-            <div class="div-input">
-                <label id="for-fio" class="black label-center" for="fio">ФИО</label>
-                <input name="fio" class="size-input" id="fio" type="text" >
-            </div>
-
-            <div class="div-input">
-                <label id="for-telephone" class="black label-center" for="telephone">Телефон</label>
-                <input name="telephone" class="size-input" id="telephone" type="tel" >
-            </div>
-
-            <div class="div-input">
-                <label id="for-email" class="black label-center" for="email">Email</label>
-                <input name="email" class="size-input" id="email" type="email" >
-            </div>
-
-            <div class="div-input">
-                <label id="for-bday" class="black label-center" for="bday">Дата рождения</label>
-                <input name="bday" class="size-input" id="bday" type="date" >
-            </div>
-
-            <div class="div-input">
-                <label id="for-organization" class="black label-center">Пол</label>
-                <div id="sex-radios" class="label-center">
-                    <input name="sex" class="" id="sex-man" type="radio" value="man" >
-                    <label id="for-sex-man" class="black label-center">Мужской</label>
-
-                    <input name="sex" class="" id="sex-woman" type="radio" value="woman" >
-                    <label id="for-sex-woman" class="black label-center">Женский</label>
-                </div>
-            </div>
-
-            <div class="div-input">
+            <fieldset>
                 <label id="for-prog-lang" class="black label-center">Любимый язык программирования</label>
                 <select name="langs[]" multiple="multiple" id="prog-lang" class="size-input" >
                     <option value="0">Pascal</option>
@@ -82,33 +78,20 @@
                     <option value="9">Prolog</option>
                     <option value="10">Scala</option>
                 </select>
+            </fieldset>
+            С контрактом ознакомлен(-а)
+                <input type="checkbox" name="contract" id="Check" checked>
+                <label for="Check"></label>
+            <div>
+              <button type="submit">Сохранить</button>
             </div>
-
-            <div class="div-input">
-                <label id="for-biography" class="black" for="biography">Биография</label>
-                <textarea class="size-input" id="biography" name="biography" ></textarea>
-            </div>
-
-            <div class="label-center">
-                <input id="contract" type="checkbox" name="contract" value="1">
-                <label id="for-contract" class="black" for="contract">С контрактом ознакомлен</label>
-            </div>
-
-            <div id="div-with-submit">
-                <input id="submit-request" class="div_button" type="submit" value="Сохранить">
-            </div>
-            <!--            <script>-->
-            <!--                document.getElementById('form').addEventListener('submit', function(event) {-->
-            <!--                    event.preventDefault();-->
-            <!--            </script>-->
-        </form>
-    </div>
-</main>
-
-<footer>
-    <p class=" text_in_footer">created by</p>
-    <h3 class=" text_in_footer">Antoha bidZ footbpli$t</h3>
-</footer>
+          </form>
+        </div>
+      <footer class = "rounded-top mt-2">
+        <div id = "footer_txt">© Tolstenkov Anton</div>
+      </footer>
+    </body>
+</html>
 </body>
 
 </html>
